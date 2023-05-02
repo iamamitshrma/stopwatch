82a5edf4f48231b1dbcb1b894a0fe19ca547bb9d
Amit Sharma <iamamitshrma@gmail.com>
2023-05-02 13:10:39 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,21 @@
-import { useRef, useState } from 'react';
-import './App.css';
-import Stopwatch from './components/Stopwatch';
-import { formatTime } from './helpers/formatTime';
+import { useState } from "react";
+import "./App.css";
+import Stopwatch from "./components/Stopwatch";
 
 function App() {
-    const [time, setTime] = useState(0);
-    const [isRunning, setIsRunning] = useState(false);
-    const intervalRef = useRef(null);
-    const startTimeRef = useRef(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const handleStartStopClick = () => {
+    setIsRunning((r) => !r);
+  };
 
-    function start() {
-      if (!isRunning) {
-        setIsRunning(true);
-        startTimeRef.current = Date.now() - time;
-        intervalRef.current = setInterval(() => {
-          setTime(Date.now() - startTimeRef.current);
-        }, 10);
-      }
-    }
-
-    function stop() {
-      if (isRunning) {
-        setIsRunning(false);
-        clearInterval(intervalRef.current);
-      }
-    }
   return (
     <div className="App">
       <div className="stopwatch_container">
         <h1 className="stopwatch_heading">Stopwatch</h1>
-        {isRunning ? <Stopwatch time={time} /> : <h1>{formatTime(time)}</h1>}
-
+        {isRunning && <Stopwatch />}
         <div className="stopwatch_buttons">
-          <button onClick={start} className="start_btn">
-            Start
-          </button>
-          <button onClick={stop} className="stop_btn">
-            Stop
+          <button className={isRunning ? "stop_btn" : "start_btn"} onClick={handleStartStopClick}>
+            {isRunning ? "Stop" : "Start"}
           </button>
         </div>
       </div>
--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import { formatTime } from '../helpers/formatTime';
+import React, { useEffect, useState } from "react";
+import { formatTime } from "../helpers/formatTime";
+import "./Stopwatch.css";
 
-const Stopwatch = ({time}) => {
+const Stopwatch = () => {
+  const time = localStorage.getItem("time");
+  const [count, setCount] = useState(time === "null" ? 0 : Number(time));
+
+  useEffect(() => {
+    let timer;
+    timer = setInterval(() => {
+      setCount((c) => c + 10);
+    }, 10);
+    return () => {
+      clearInterval(timer);
+      localStorage.setItem("time", count)
+    };
+  }, [count]);
+
   return (
     <div>
-      <h1>{formatTime(time)}</h1>
+      <h1 className="stopwatch_counter">{formatTime(count)}</h1>
     </div>
   );
-}
+};
 
-export default Stopwatch
+export default Stopwatch;
